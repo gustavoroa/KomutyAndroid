f0b24660beade4286bd62be55bdb4dd4ec1999ef
Jose Gustavo Roa de la Fuente <roasystems@gmail.com>
2024-12-20 23:44:25 -0800

    SessionManager class added. This is required to identify all the time if the user has a session started

---
+++ app/src/main/java/com/roasystems/komuty/utility/SessionManager.java
@@ -0,0 +1,26 @@
+package com.roasystems.komuty.utility;
+import android.content.Context;
+import android.content.SharedPreferences;
+public class SessionManager {
+    private static final String PREF_NAME = "user_session"; // SharedPreferences file name
+    private static final String KEY_IS_LOGGED_IN = "is_logged_in"; // Key to store login status
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    public SessionManager(Context context) {
+        sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE); // Initialize SharedPreferences
+        editor = sharedPreferences.edit(); // Get editor to modify SharedPreferences
+    }
+
+    // Save login status
+    public void setLoggedIn(boolean isLoggedIn) {
+        editor.putBoolean(KEY_IS_LOGGED_IN, isLoggedIn); // Put the login status in SharedPreferences
+        editor.apply(); // Commit changes asynchronously
+    }
+
+    // Get login status
+    public boolean isLoggedIn() {
+        return sharedPreferences.getBoolean(KEY_IS_LOGGED_IN, false); // Return login status; false by default
+    }
+}
